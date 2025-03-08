b155d149da28e8da1a2e4ad16f42fbc79767680b
Vi <viviantran210@gmail.com>
2025-03-07 22:49:24 -0500

    added new PDU

--- hw4-udp/du-ftp.h
+++ hw4-udp/du-ftp.h
@@ -13,3 +13,19 @@ typedef struct prog_config{
     char    svr_ip_addr[16];
     char    file_name[128];
 } prog_config;
+
+#define     DP_S_IN_PROGRESS          0
+#define     DP_S_COMPLETE             1
+#define     DP_S_ERROR               -1
+#define     DP_ERROR_FILE_OPEN       -33 // cannot open file
+
+// Define the PDU for the FTP protocol
+typedef struct dp_ftp_pdu {
+    int     proto_ver;
+    int     mtype;    
+    char    file_name[128]; // File name being transferred
+    int     status;         // Status of the transfer (0 = in progress, 1 = complete)
+    int     seqnum;
+    int     err_num;   
+    int     dgram_sz;    
+} dp_ftp_pdu;
